c8dc6d49cd008364a2cf59c9228fa3bcfeff0672
vidfel <vidal.luisfelipe@gmail.com>
2019-08-03 05:45:17 +0000

    Update .bat Files

--- _PushToMT4Dev.bat
+++ _PushToMT4Dev.bat
@@ -5,9 +5,9 @@ rem Use in case some content needs to be replaced (reverted from Version Control
 setlocal enabledelayedexpansion
 
 :: Source Directory where Version Control Repository is located
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\WatchDog"
+set SOURCE_DIR="C:\Users\vidfel\Documents\GitHub\WatchDog"
 :: Destination Directory where Expert Advisor is located
-set DEST_DIR="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\00_WatchDog"
+set DEST_DIR="C:\Program Files (x86)\OANDA - MetaTrader - Terminal2\MQL4\Experts\00_WatchDog"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR% *.mq4
 
